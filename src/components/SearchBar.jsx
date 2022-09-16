3d46069293293a47107c73215382ebd1829dc4c6
ann <asog.jrti@gmail.com>
2022-09-16 16:54:25 +0200

    add mui icons and styles search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {setFilteredSmartphones, smartphonesListSelector } from '../state/smartphones/smartphonesSlice';
 import styles from './SearchBar.module.scss';
+import SearchIcon from '@mui/icons-material/Search';
 
 export const SearchBar = () => {
     const allSmartphones = useSelector(smartphonesListSelector);
@@ -20,12 +21,13 @@ export const SearchBar = () => {
 
     return(
         <div className={styles.searchBarContainer}>
-        <input
-          className={styles.searchBar}
-          onChange={filter}
-          type="text"
-          placeholder="Search for brand or model"
-        ></input>
+        <SearchIcon style={{color: '#BC6C25'}} />
+          <input
+            className={styles.searchBarInput}
+            onChange={filter}
+            type="text"
+            placeholder="Search for brand or model"
+          ></input>
       </div>
     )
 }
