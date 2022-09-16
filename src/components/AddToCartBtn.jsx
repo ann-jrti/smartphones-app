2dfe673484f971ffca12cba8ec7c1fab1a297cdb
ann <asog.jrti@gmail.com>
2022-09-16 17:02:51 +0200

    styles add cart btn

--- src/components/AddToCartBtn.jsx
+++ src/components/AddToCartBtn.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import styles from './AddToCartBtn.module.scss';
-
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 export const AddToCartBtn = () => {
     return(
-        <button type="submit"className={styles.addCartBtn}>Add to cart</button>
+        <button type="submit"className={styles.addCartBtn}>
+            Add to cart
+            <AddShoppingCartIcon style={{color: '#283618'}} />
+        </button>
     )
 }
